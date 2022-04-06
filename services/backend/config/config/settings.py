6b980433ad71aa2c9d28b6906f27f50bf6c9a310
Robert Paul <rob.k.paul@gmail.com>
2022-04-06 13:30:49 -0700

    fixed secret key leak

--- services/backend/config/config/settings.py
+++ services/backend/config/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3ksb9*^tv@1skvhik+c9gyte-91xn8(--ucty7ei0y=y%uegph'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
