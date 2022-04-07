bc51d769b967fc4068d5aa71ed513bb6a74d9475
robkpaul <rob.k.paul@gmail.com>
2022-04-06 21:54:28 -0700

    api test

--- services/backend/src/config/urls.py
+++ services/backend/src/config/urls.py
@@ -26,7 +26,6 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
 ]
