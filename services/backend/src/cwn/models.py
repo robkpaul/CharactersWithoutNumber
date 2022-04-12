1dc3b871d1266192f3b319d454402ab34a0491c9
Robert Paul <rob.k.paul@gmail.com>
2022-04-12 11:28:33 -0700

    initial migrations

--- services/backend/src/cwn/models.py
+++ services/backend/src/cwn/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Character():
+class Character(models.Model):
     name = models.CharField(max_length=127)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    campaign = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    campaign = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="characters")
 
-class Campaign():
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+class Campaign(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_campaigns")
     players = models.ManyToManyField(User)
