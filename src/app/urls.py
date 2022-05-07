4878b2c76173860836e292046ad0dfe038f72f9a
robkpaul <rob.k.paul@gmail.com>
2022-05-07 16:29:21 -0700

    remove dicelog url

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('dicelog', views.dicelog),
     path('campaign/<int:campaign_id>', views.campaign),
     path('character/<int:character_id>', views.character)
 ]
